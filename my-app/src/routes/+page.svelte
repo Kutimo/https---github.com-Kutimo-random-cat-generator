e4303877193db2f78ca827181bac1ba2d759d4d8
Kutimo <marius-oliver@hotmail.com>
2022-10-17 11:56:17 +0200

    cat generator of name and breed.

---
+++ my-app/src/routes/+page.svelte
@@ -0,0 +1,44 @@
+<script>
+import Button from "./Button.svelte"
+import { onMount } from "svelte";
+
+</script>
+    
+    <body>
+        <main>
+        <div class="generator">
+            <h2>Cat name and breed generator</h2>
+            <Button></Button>
+            </div>
+            </main>
+
+    </body>
+
+
+<style>
+    :global(*) {
+        box-sizing: border-box;
+        margin: 0;
+        padding: 0%;
+    }
+    main {
+        height: 100vh;
+        width: 100%;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+    .generator {
+        display: flex;
+        flex-direction: column;
+        justify-content: space-evenly;
+        align-items: center;
+        height: 20%;
+        width: 30%;
+        background-color: gainsboro;
+    }
+    h2 {
+        text-align: center;
+        font-size: 2rem;
+    }
+</style>
---
+++ my-app/src/routes/Button.svelte
@@ -0,0 +1,42 @@
+<script>
+    const CatNameAndBreed = ['catName', 'breed'];
+ let data = {
+    catData: undefined,
+    loading: false,
+ };
+ /* let CatNameAndBreed; */
+    async function LoadData() {
+        data.loading = true;
+        const response = await fetch('/random-cat')
+        const CatNameAndBreed = await response.json();
+        data = {
+            catData: CatNameAndBreed,
+            loading: false,
+        };
+    }
+    console.log(CatNameAndBreed) 
+</script>
+
+<button on:click={LoadData}>Generate Breed and Name!</button>
+{#if data.loading === true}
+loading cat name and breed...
+{:else if data.catData != undefined}
+    {data.catData}
+    {/if}
+<style>
+    button{
+        color: white;
+        background-color: black;
+        height: 50px;
+        width: 250px;
+        font-size: 1rem;
+        padding: 10px;
+    }
+    button:hover {
+        cursor: pointer;
+        color: black;
+        background-color: white;
+        box-shadow: 3px 3px 0px 3px rgba(0, 0, 0, 0.75);
+       transition: ease-in 300ms;
+    }
+</style>
